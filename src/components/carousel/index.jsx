eb405f94ad83a34926a20c107575cc7b06a4ad14
siavhnz <siavhnz@gmail.com>
2023-05-21 01:27:53 +0330

    work on mobile design

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -4,27 +4,28 @@ import { ReactComponent as NextIcon } from "../../assets/images/icon-next.svg";
 import { ReactComponent as PrevIcon } from "../../assets/images/icon-prev.svg";
 
 const Carousel = () => {
-    return <div>
+    return <div className={styles.carousel}>
         {
             data.map((item, index) => {
-                return <div key={index}>
-                    <div>
-                        <img src={item.image} alt={item.name} />
-                        <div>
-                            <button aria-label="prev slide">
-                                <PrevIcon />
-                            </button>
-                            <button aria-label="next slide">
-                                <NextIcon />
-                            </button>
+                return <div key={index} className={styles["inner-carousel"]}>
+                    <div className={styles["img-container"]}>
+                        <div className={styles["inner-img-container"]}>
+                            <img src={item.image} alt={item.name} />
+                            <div className={styles.handlers}>
+                                <button aria-label="prev slide">
+                                    <PrevIcon />
+                                </button>
+                                <button aria-label="next slide">
+                                    <NextIcon />
+                                </button>
+                            </div>
                         </div>
                     </div>
-                    <div>
-                        <p>{item.testimonial}</p>
-                        <p>{item.name}</p>
-                        <p>{item.role}</p>
+                    <div className={styles.content}>
+                        <p className={styles.testi}>“ {item.testimonial} ”</p>
+                        <p className={styles.name}>{item.name}</p>
+                        <p className={styles.role}>{item.role}</p>
                     </div>
-
                 </div>
             })
         }
--- src/store/data.js
+++ src/store/data.js
@@ -7,12 +7,12 @@ export const data = [
         I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
         excited about the future.`
     },
-    {
-        name: "John Tarkpor",
-        image: "/images/image-john.jpg",
-        role: "Junior Front-end Developer",
-        testimonial: `If you want to lay the best foundation possible I’d recommend taking this course. 
-        The depth the instructors go into is incredible. I now feel so confident about 
-        starting up as a professional developer.`
-    }
+    // {
+    //     name: "John Tarkpor",
+    //     image: "/images/image-john.jpg",
+    //     role: "Junior Front-end Developer",
+    //     testimonial: `If you want to lay the best foundation possible I’d recommend taking this course. 
+    //     The depth the instructors go into is incredible. I now feel so confident about 
+    //     starting up as a professional developer.`
+    // }
 ]
